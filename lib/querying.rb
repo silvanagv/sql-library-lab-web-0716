401831e6516a0eb005aa1680e4cdc2626a5526c8
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-04 17:43:06 -0400

    finished pair lab

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,15 +31,24 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species)
+  "SELECT series.title
   FROM series
   JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species='human'
+  GROUP BY series.title
   ORDER BY COUNT(characters.species) DESC
-
+  LIMIT 1
   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(books.id)
+  FROM character_books
+  INNER JOIN books
+  ON books.id = character_books.book_id
+  INNER JOIN characters
+  ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(characters.name) DESC"
 end
